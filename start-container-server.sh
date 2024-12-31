6d9da92d6b64029920637b330b8ecfff751e7a18
drewburr <drewburr9@gmail.com>
2024-12-31 13:26:30 -0500

    Add file templating from environment variables

--- start-container-server.sh
+++ start-container-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CLUSTER_ROOT="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster"
 
 # Check for game updates before each start. If the game client updates and your server is out of date, you won't be
 # able to see it on the server list. If that happens just restart the containers and you should get the latest version
@@ -8,19 +9,38 @@ then
 fi
 
 # Copy dedicated_server_mods_setup.lua
-ds_mods_setup="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
+ds_mods_setup="$CLUSTER_ROOT/mods/dedicated_server_mods_setup.lua"
 if [ -f "$ds_mods_setup" ]
 then
   cp $ds_mods_setup "$HOME/server_dst/mods/"
 fi
 
 # Copy modoverrides.lua
-modoverrides="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
+modoverrides="$CLUSTER_ROOT/mods/modoverrides.lua"
 if [ -f "$modoverrides" ]
 then
-  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
-  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
+  cp $modoverrides "$CLUSTER_ROOT/Master/"
+  cp $modoverrides "$CLUSTER_ROOT/Caves/"
 fi
 
+if [ -n "$cluster_token" ]
+then
+  echo "Setting cluster token from environment"
+  echo "$cluster_token" > $CLUSTER_ROOT/cluster_token.txt
+fi
+
+if [ -n "$adminlist" ]
+then
+  cat DSTConfigTemplates/adminlist.txt | envsubst > "$CLUSTER_ROOT/adminlist.txt"
+fi
+
+if [ -n "$blocklist" ]
+then
+  cat DSTConfigTemplates/blocklist.txt | envsubst > "$CLUSTER_ROOT/blocklist.txt"
+fi
+# Template config from environment variables
+cat DSTConfigTemplates/cluster.ini | envsubst > "$CLUSTER_ROOT/cluster.ini"
+
+exit
 cd $HOME/server_dst/bin
 ./dontstarve_dedicated_server_nullrenderer -cluster DSTWhalesCluster -shard "$SHARD_NAME"
